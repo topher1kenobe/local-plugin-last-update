03e41bb93578bd00d8bf4349dd3b38869ce35fb0
Topher DeRosia <topher@mediaforge.pro>
2026-05-15 09:55:05 -0400

    Adjust version number

--- local-last-update.php
+++ local-last-update.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Local Last Update
  * Description:       Displays the date that plugins were last updated on this site, with a sortable column header.
- * Version:           2.0.0
+ * Version:           2.0.1
  * Author:            Topher
  * Requires at least: 5.0
  * Requires PHP:      7.2
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@
 **Requires at least:** 5.0
 **Tested up to:** 6.9
 **Requires PHP:** 7.2
-**Stable tag:** 2.0.0
+**Stable tag:** 2.0.1
 **License:** GPLv2 or later
 **License URI:** https://www.gnu.org/licenses/gpl-2.0.html
 
